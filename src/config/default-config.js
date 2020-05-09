95dd1131f5e3b63b3807849cd69a108a2b1467e4
Lucas Portet <lucasportet@gmail.com>
2020-05-09 17:18:44 +0200

    docs(config): Add comments in default config.

--- src/config/default-config.js
+++ src/config/default-config.js
@@ -1,3 +1,9 @@
+/* This is a default config file.
+** Don't modify it unless you want to set specific default values.
+** If you want to create a custom config file,
+** write it at the root of the project as `config.js`
+*/
+
 module.exports = {
   scraper: {
     headless: true
